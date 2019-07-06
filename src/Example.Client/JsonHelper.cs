2a77c61d27efe39d76315abe83bddd1fccaac020
Eugene Bekker <ebekker@users.noreply.github.com>
2019-07-06 12:10:05 -0400

    adding support for row selection

---
+++ src/Example.Client/JsonHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Example.Client
+{
+    public static class JsonHelper
+    {
+        public static readonly JsonSerializerOptions DefaultSerOptions = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        };
+
+        public static T Parse<T>(string json)
+        {
+            return JsonSerializer.Parse<T>(json, DefaultSerOptions);
+        }
+
+        public static string ToString<T>(T value)
+        {
+            return JsonSerializer.ToString<T>(value);
+        }
+    }
+}
---
+++ src/Example.Client/Pages/FetchData2Model.cs
@@ -0,0 +1,13 @@
+﻿using Example.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Example.Client.Pages
+{
+    public class FetchData2Model
+    {
+        public WeatherForecast CurrentForecast { get; set; }
+    }
+}
